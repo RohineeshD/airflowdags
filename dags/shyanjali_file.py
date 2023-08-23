7b4ebe22a775749b24a9694e27cd50d9c0e804ed
Shyanjali4 <141653202+Shyanjali4@users.noreply.github.com>
2023-08-23 16:36:55 +0530

    Update shyanjali_file.py

--- dags/shyanjali_file.py
+++ dags/shyanjali_file.py
@@ -1,54 +1,30 @@
-from datetime import datetime
+# Step 1: Importing Modules
+# To initiate the DAG Object
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+# Importing datetime and timedelta modules for scheduling the DAGs
+from datetime import timedelta, datetime
+# Importing operators 
+from airflow.operators.dummy_operator import DummyOperator
 
-# Define the functions for tasks
-def multiply_numbers(**kwargs):
-    num1 = kwargs['num1']
-    num2 = kwargs['num2']
-    result = num1 * num2
-    print(f"Multiplication Result: {result}")
-
-def add_numbers(**kwargs):
-    num1 = kwargs['num1']
-    num2 = kwargs['num2']
-    result = num1 + num2
-    print(f"Addition Result: {result}")
-
-# Define the default arguments for the DAG
+# Step 2: Initiating the default_args
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 8, 23),
-    'retries': 1,
+        'owner' : 'airflow',
+        'start_date' : datetime(2022, 11, 12),
+
 }
 
-# Create the DAG instance
-dag = DAG(
-    'shyanjali_dag',
-    default_args=default_args,
-    schedule_interval=None,  # Set to a specific interval (e.g., '0 0 * * *') if needed
-)
+# Step 3: Creating DAG Object
+dag = DAG(dag_id='manik_dag',
+        default_args=default_args,
+        schedule_interval='@once', 
+        catchup=False
+    )
 
-# Define the tasks
-num1 = 5
-num2 = 3
+# Step 4: Creating task
+# Creating first task
+start = DummyOperator(task_id = 'start', dag = dag)
+# Creating second task 
+end = DummyOperator(task_id = 'end', dag = dag)
 
-multiply_task = PythonOperator(
-    task_id='multiply_task',
-    python_callable=multiply_numbers,
-    op_args=[num1, num2],
-    provide_context=True,
-    dag=dag,
-)
-
-add_task = PythonOperator(
-    task_id='add_task',
-    python_callable=add_numbers,
-    op_args=[num1, num2],
-    provide_context=True,
-    dag=dag,
-)
-
-# Set task dependencies
-multiply_task >> add_task
+ # Step 5: Setting up dependencies 
+start >> end 
