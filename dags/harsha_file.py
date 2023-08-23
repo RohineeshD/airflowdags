399199a66fbcc18d6294911540f133258cbe9343
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-23 17:52:03 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -1,70 +1,70 @@
-from airflow import DAG
-from airflow.providers.snowflake.transfers.s3_to_snowflake import SnowflakeOperator
-from datetime import datetime, timedelta
-from airflow.operators.dummy_operator import DummyOperator
-
-# Step 2: Initiating the default_args
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2022, 11, 12),
-}
-
-# Step 3: Creating DAG Object
-dag = DAG(
-    dag_id='harsha_dag',
-    default_args=default_args,
-    schedule_interval='@hourly',
-    catchup=False,
-)
-
-# Step 4: Creating tasks
-start = DummyOperator(task_id='start', dag=dag)
-
-# Retrieving the maximum patient ID using SnowflakeOperator
-retrieve_max_patient_id = SnowflakeOperator(
-    task_id='retrieve_max_patient_id',
-    sql="SELECT MAX(patient_id) FROM patients;",
-    snowflake_conn_id='snowflake_conn',  # Specify your Snowflake connection ID
-    autocommit=True,  # Auto-commit the transaction
-    dag=dag,
-)
-
-# Creating second task
-end = DummyOperator(task_id='end', dag=dag)
-
-# Setting up dependencies
-start >> retrieve_max_patient_id >> end
-
-
-
-
-# # Step 1: Importing Modules
-# # To initiate the DAG Object
 # from airflow import DAG
-# # Importing datetime and timedelta modules for scheduling the DAGs
-# from datetime import timedelta, datetime
-# # Importing operators 
+# from airflow.providers.snowflake.transfers.s3_to_snowflake import SnowflakeOperator
+# from datetime import datetime, timedelta
 # from airflow.operators.dummy_operator import DummyOperator
 
 # # Step 2: Initiating the default_args
 # default_args = {
-#         'owner' : 'airflow',
-#         'start_date' : datetime(2022, 11, 12),
-
+#     'owner': 'airflow',
+#     'start_date': datetime(2022, 11, 12),
 # }
 
 # # Step 3: Creating DAG Object
-# dag = DAG(dag_id='harsha_dag',
-#         default_args=default_args,
-#         schedule_interval='@hourly', 
-#         catchup=False
-#     )
+# dag = DAG(
+#     dag_id='harsha_dag',
+#     default_args=default_args,
+#     schedule_interval='@hourly',
+#     catchup=False,
+# )
 
-# # Step 4: Creating task
-# # Creating first task
-# start = DummyOperator(task_id = 'start', dag = dag)
-# # Creating second task 
-# end = DummyOperator(task_id = 'end', dag = dag)
+# # Step 4: Creating tasks
+# start = DummyOperator(task_id='start', dag=dag)
 
-#  # Step 5: Setting up dependencies 
-# start >> end 
+# # Retrieving the maximum patient ID using SnowflakeOperator
+# retrieve_max_patient_id = SnowflakeOperator(
+#     task_id='retrieve_max_patient_id',
+#     sql="SELECT MAX(patient_id) FROM patients;",
+#     snowflake_conn_id='snowflake_conn',  # Specify your Snowflake connection ID
+#     autocommit=True,  # Auto-commit the transaction
+#     dag=dag,
+# )
+
+# # Creating second task
+# end = DummyOperator(task_id='end', dag=dag)
+
+# # Setting up dependencies
+# start >> retrieve_max_patient_id >> end
+
+
+
+
+# Step 1: Importing Modules
+# To initiate the DAG Object
+from airflow import DAG
+# Importing datetime and timedelta modules for scheduling the DAGs
+from datetime import timedelta, datetime
+# Importing operators 
+from airflow.operators.dummy_operator import DummyOperator
+
+# Step 2: Initiating the default_args
+default_args = {
+        'owner' : 'airflow',
+        'start_date' : datetime(2022, 11, 12),
+
+}
+
+# Step 3: Creating DAG Object
+dag = DAG(dag_id='harsha_dag',
+        default_args=default_args,
+        schedule_interval='@hourly', 
+        catchup=False
+    )
+
+# Step 4: Creating task
+# Creating first task
+start = DummyOperator(task_id = 'start', dag = dag)
+# Creating second task 
+end = DummyOperator(task_id = 'end', dag = dag)
+
+ # Step 5: Setting up dependencies 
+start >> end 
