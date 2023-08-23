9a225a855af60237b5cc1b840e90a5391d889b05
Shyanjali4 <141653202+Shyanjali4@users.noreply.github.com>
2023-08-23 16:13:09 +0530

    Update shyanjali_file.py

--- dags/shyanjali_file.py
+++ dags/shyanjali_file.py
@@ -4,7 +4,20 @@ from airflow import DAG
 # Importing datetime and timedelta modules for scheduling the DAGs
 from datetime import timedelta, datetime
 # Importing operators 
-from airflow.operators.dummy_operator import DummyOperator
+# from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def add_numbers(**kwargs):
+    num1 = kwargs['num1']
+    num2 = kwargs['num2']
+    result = num1 + num2
+    print(f"Addition Result: {num1} + {num2} = {result}")
+
+def multiply_numbers(**kwargs):
+    num1 = kwargs['num1']
+    num2 = kwargs['num2']
+    result = num1 * num2
+    print(f"Multiplication Result: {num1} * {num2} = {result}")
 
 # Step 2: Initiating the default_args
 default_args = {
@@ -20,11 +33,29 @@ dag = DAG(dag_id='shyanjali_dag',
         catchup=False
     )
 
+with DAG('math_operations_dag', default_args=default_args, schedule_interval=timedelta(days=1)) as dag:
+    task_addition = PythonOperator(
+        task_id='addition_task',
+        python_callable=add_numbers,
+        op_args=[{'num1': 5, 'num2': 7}],
+        provide_context=True,
+    )
+
+    task_multiplication = PythonOperator(
+        task_id='multiplication_task',
+        python_callable=multiply_numbers,
+        op_args=[{'num1': 3, 'num2': 4}],
+        provide_context=True,
+    )
+
+    task_addition >> task_multiplication
+
+
 # Step 4: Creating task
 # Creating first task
-start = DummyOperator(task_id = 'start', dag = dag)
+# start = DummyOperator(task_id = 'start', dag = dag)
 # Creating second task 
-end = DummyOperator(task_id = 'end', dag = dag)
+# end = DummyOperator(task_id = 'end', dag = dag)
 
  # Step 5: Setting up dependencies 
-start >> end 
+# start >> end 
