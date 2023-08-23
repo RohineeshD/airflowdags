7fc0175fb9c3088f121b84f66028cdff9ebe2193
Shyanjali4 <141653202+Shyanjali4@users.noreply.github.com>
2023-08-23 16:50:37 +0530

    Update shyanjali_file.py

--- dags/shyanjali_file.py
+++ dags/shyanjali_file.py
@@ -7,7 +7,7 @@ def print_hello():
     return 'Hello world from first Airflow DAG!'
 
 dag = DAG('shyanjali_dag',
-          schedule_interval='@once,
+          schedule_interval='@once',
           start_date=datetime(2022, 11, 12), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
