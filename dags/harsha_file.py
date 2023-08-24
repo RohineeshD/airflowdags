2c22dbcf6fe8797c376dd70a788f3006c368326a
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-24 22:44:51 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -1,23 +1,91 @@
+# from airflow import DAG
+# from airflow.providers.http.sensors.http import HttpSensor
+# from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+# from airflow.operators.python_operator import PythonOperator
+# from datetime import datetime, timedelta
+# import pandas as pd
+# import requests
+# import tempfile
+
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2023, 8, 25),
+#     'retries': 1,
+# }
+
+# dag = DAG(
+#     'airline_safety_dag',
+#     default_args=default_args,
+#     schedule_interval=None,  # Set to None for manual triggering
+#     catchup=False,
+# )
+
+# def check_env_variable(**kwargs):
+#     if kwargs['dag_run'].conf.get('load_data'):
+#         return 'load_data_task'
+
+# check_env_task = PythonOperator(
+#     task_id='check_env_task',
+#     python_callable=check_env_variable,
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# def read_data_and_load_to_snowflake(**kwargs):
+#     url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv"
+#     response = requests.get(url)
+#     data = response.text
+    
+#     # Create a temporary file to store the data
+#     with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_file:
+#         temp_file.write(data.encode())
+#         temp_file_path = temp_file.name
+
+#     # Load data into Snowflake using SnowflakeOperator
+#     sql_query = f"""
+#     COPY INTO airflow_tasks
+#     FROM '{temp_file_path}'
+#     FILE_FORMAT = (TYPE = CSV)
+#     """
+    
+#     snowflake_task = SnowflakeOperator(
+#         task_id='load_data_into_snowflake',
+#         sql=sql_query,
+#         snowflake_conn_id='snowflake_conn',  # Specify your Snowflake connection ID
+#         autocommit=True,
+#         dag=dag,
+#     )
+    
+#     snowflake_task.execute(context=kwargs)
+
+# load_data_task = PythonOperator(
+#     task_id='load_data_task',
+#     python_callable=read_data_and_load_to_snowflake,
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# check_env_task >> load_data_task
+
+
 from airflow import DAG
-from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-import pandas as pd
 import requests
 import tempfile
 import logging
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2022, 11, 12),
+    'start_date': datetime(2023, 8, 25),
     'retries': 1,
 }
 
 dag = DAG(
-    'airline_safety_dag_harsha',
+    'airline_safety_dag_harsha1',
     default_args=default_args,
-    schedule_interval='@once',  # Set to None for manual triggering
+    schedule_interval=None,  # Set to None for manual triggering
     catchup=False,
 )
 
@@ -25,7 +93,7 @@ def check_env_variable(**kwargs):
     if kwargs['dag_run'].conf.get('load_data'):
         return 'load_data_task'
 
-check_env_task = PythonOperator(
+check_env_task1 = PythonOperator(
     task_id='check_env_task',
     python_callable=check_env_variable,
     provide_context=True,
@@ -45,34 +113,111 @@ def read_data_and_load_to_snowflake(**kwargs):
             temp_file.write(data.encode())
             temp_file_path = temp_file.name
 
-        # Load data into Snowflake using SnowflakeOperator
-        sql_query = f"""
-        COPY INTO airflow_tasks
+        # Load data into Snowflake
+        # Specify your Snowflake connection ID and other parameters
+        snowflake_conn_id = 'snowflake_conn'
+        table_name = 'airflow_tasks'
+        schema_name = 'exusia_schema'
+
+        load_query = f"""
+        COPY INTO {schema_name}.{table_name}
         FROM '{temp_file_path}'
         FILE_FORMAT = (TYPE = CSV)
         """
-
-        snowflake_task = SnowflakeOperator(
-            task_id='load_data_into_snowflake',
-            sql=sql_query,
-            snowflake_conn_id='snowflake_conn',  # Specify your Snowflake connection ID
-            autocommit=True,
-            dag=dag,
-        )
-
-        snowflake_task.execute(context=kwargs)
+        
+        # Execute the query using the SnowflakeHook
+        snowflake_hook = SnowflakeHook(snowflake_conn_id=snowflake_conn_id)
+        snowflake_hook.run(load_query)
+        
         logging.info("Data loaded into Snowflake successfully.")
     except requests.exceptions.RequestException as e:
         logging.error(f"Failed to fetch data from URL: {e}")
 
-load_data_task = PythonOperator(
+load_data_task2 = PythonOperator(
     task_id='load_data_task',
     python_callable=read_data_and_load_to_snowflake,
     provide_context=True,
     dag=dag,
 )
 
-check_env_task >> load_data_task
+check_env_task1 >> load_data_task2
+
+
+# from airflow import DAG
+# from airflow.providers.http.sensors.http import HttpSensor
+# from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+# from airflow.operators.python_operator import PythonOperator
+# from datetime import datetime, timedelta
+# import pandas as pd
+# import requests
+# import tempfile
+# import logging
+
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2022, 11, 12),
+#     'retries': 1,
+# }
+
+# dag = DAG(
+#     'airline_safety_dag_harsha',
+#     default_args=default_args,
+#     schedule_interval='@once',  # Set to None for manual triggering
+#     catchup=False,
+# )
+
+# def check_env_variable(**kwargs):
+#     if kwargs['dag_run'].conf.get('load_data'):
+#         return 'load_data_task'
+
+# check_env_task = PythonOperator(
+#     task_id='check_env_task',
+#     python_callable=check_env_variable,
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# def read_data_and_load_to_snowflake(**kwargs):
+#     url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv"
+    
+#     try:
+#         response = requests.get(url)
+#         response.raise_for_status()  # Raise exception for non-200 responses
+#         data = response.text
+
+#         # Create a temporary file to store the data
+#         with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_file:
+#             temp_file.write(data.encode())
+#             temp_file_path = temp_file.name
+
+#         # Load data into Snowflake using SnowflakeOperator
+#         sql_query = f"""
+#         COPY INTO airflow_tasks
+#         FROM '{temp_file_path}'
+#         FILE_FORMAT = (TYPE = CSV)
+#         """
+
+#         snowflake_task = SnowflakeOperator(
+#             task_id='load_data_into_snowflake',
+#             sql=sql_query,
+#             snowflake_conn_id='snowflake_conn',  # Specify your Snowflake connection ID
+#             autocommit=True,
+#             dag=dag,
+#         )
+
+#         snowflake_task.execute(context=kwargs)
+#         logging.info("Data loaded into Snowflake successfully.")
+#     except requests.exceptions.RequestException as e:
+#         logging.error(f"Failed to fetch data from URL: {e}")
+
+# load_data_task = PythonOperator(
+#     task_id='load_data_task',
+#     python_callable=read_data_and_load_to_snowflake,
+#     provide_context=True,
+#     dag=dag,
+# )
+
+# check_env_task >> load_data_task
 
 
 
