7a3dc97aef90fd394775b1b330db7ec43f47e053
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-24 17:52:52 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -57,3 +57,33 @@ snowflake_task = SnowflakeOperator(
 #     autocommit=True,
 #     dag=dag,
 # )
+
+# from airflow import DAG
+# from datetime import datetime, timedelta
+# from airflow.operators.python_operator import PythonOperator
+
+# # Step 2: Initiating the default_args
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2022, 11, 12),
+# }
+
+# # Define a Python function to be executed by the PythonOperator
+# def print_hello():
+#     print("Hello from the PythonOperator!")
+
+# # Step 3: Creating DAG Object
+# dag = DAG(
+#     dag_id='harsha_dag',
+#     default_args=default_args,
+#     schedule_interval='@once',  
+#     catchup=False,
+# )
+
+# # Step 4: Creating task
+# # Create a PythonOperator that will run the print_hello function
+# task = PythonOperator(
+#     task_id='print_hello_task',
+#     python_callable=print_hello,
+#     dag=dag,
+# )
