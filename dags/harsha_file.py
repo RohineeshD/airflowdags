5d1f34942027684de1a33314452eea65b3e82123
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-24 23:02:02 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -16,7 +16,7 @@ default_args = {
 dag = DAG(
     'airline_safety_dag_harsha1',
     default_args=default_args,
-    schedule_interval='none',  # Set to None for manual triggering
+    schedule_interval=None,  # Set to None for manual triggering
     catchup=False,
 )
 
