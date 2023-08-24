b6132d2862b4c50d045842ebee040be941f3601e
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-24 17:54:36 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -1,31 +1,31 @@
-# from airflow import DAG
-# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-# from datetime import datetime
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from datetime import datetime
 
-# default_args = {
-#     'owner': 'airflow',
-#     'start_date': datetime(2023, 1, 1),
-#     'retries': 1,
-# }
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+}
 
-# dag = DAG(
-#     'harsha_dag',  
-#     default_args=default_args,
-#     schedule_interval='@once',
-#     catchup=False,
-# )
+dag = DAG(
+    'harsha_dag',  
+    default_args=default_args,
+    schedule_interval='@once',
+    catchup=False,
+)
 
-# sql_query = """
-# SELECT * FROM patients WHERE status = 'Recovered'
-# """
+sql_query = """
+SELECT * FROM patients WHERE status = 'Recovered'
+"""
 
-# snowflake_task = SnowflakeOperator(
-#     task_id='execute_snowflake_query',
-#     sql=sql_query,
-#     snowflake_conn_id='snowflake_conn',
-#     autocommit=True,
-#     dag=dag,
-# )
+snowflake_task = SnowflakeOperator(
+    task_id='execute_snowflake_query',
+    sql=sql_query,
+    snowflake_conn_id='snowflake_conn',
+    autocommit=True,
+    dag=dag,
+)
 
 
 
@@ -58,32 +58,32 @@
 #     dag=dag,
 # )
 
-from airflow import DAG
-from datetime import datetime, timedelta
-from airflow.operators.python_operator import PythonOperator
+# from airflow import DAG
+# from datetime import datetime, timedelta
+# from airflow.operators.python_operator import PythonOperator
 
-# Step 2: Initiating the default_args
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2022, 11, 12),
-}
+# # Step 2: Initiating the default_args
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2022, 11, 12),
+# }
 
-# Define a Python function to be executed by the PythonOperator
-def print_hello():
-    print("Hello from the PythonOperator!")
+# # Define a Python function to be executed by the PythonOperator
+# def print_hello():
+#     print("Hello from the PythonOperator!")
 
-# Step 3: Creating DAG Object
-dag = DAG(
-    dag_id='harsha_dag',
-    default_args=default_args,
-    schedule_interval='@once',  
-    catchup=False,
-)
+# # Step 3: Creating DAG Object
+# dag = DAG(
+#     dag_id='harsha_dag',
+#     default_args=default_args,
+#     schedule_interval='@once',  
+#     catchup=False,
+# )
 
-# Step 4: Creating task
-# Create a PythonOperator that will run the print_hello function
-task = PythonOperator(
-    task_id='print_hello_task',
-    python_callable=print_hello,
-    dag=dag,
-)
+# # Step 4: Creating task
+# # Create a PythonOperator that will run the print_hello function
+# task = PythonOperator(
+#     task_id='print_hello_task',
+#     python_callable=print_hello,
+#     dag=dag,
+# )
