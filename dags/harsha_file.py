8026d5ba09593b0bf3bd7fdf85c61c8a91b55083
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-24 23:04:14 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -1,77 +1,77 @@
 
-from airflow import DAG
-from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
-from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta
-import requests
-import tempfile
-import logging
+# from airflow import DAG
+# from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+# from airflow.operators.python_operator import PythonOperator
+# from datetime import datetime, timedelta
+# import requests
+# import tempfile
+# import logging
 
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023, 8, 25),
-    'retries': 1,
-}
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2023, 8, 25),
+#     'retries': 1,
+# }
 
-dag = DAG(
-    'airline_safety_dag_harsha1',
-    default_args=default_args,
-    schedule_interval=None,  # Set to None for manual triggering
-    catchup=False,
-)
+# dag = DAG(
+#     'airline_safety_dag_harsha1',
+#     default_args=default_args,
+#     schedule_interval=None,  # Set to None for manual triggering
+#     catchup=False,
+# )
 
-def check_env_variable(**kwargs):
-    if kwargs['dag_run'].conf.get('load_data'):
-        return 'load_data_task'
+# def check_env_variable(**kwargs):
+#     if kwargs['dag_run'].conf.get('load_data'):
+#         return 'load_data_task'
 
-check_env_task1 = PythonOperator(
-    task_id='check_env_task',
-    python_callable=check_env_variable,
-    provide_context=True,
-    dag=dag,
-)
+# check_env_task1 = PythonOperator(
+#     task_id='check_env_task',
+#     python_callable=check_env_variable,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-def read_data_and_load_to_snowflake(**kwargs):
-    url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv"
+# def read_data_and_load_to_snowflake(**kwargs):
+#     url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv"
     
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise exception for non-200 responses
-        data = response.text
+#     try:
+#         response = requests.get(url)
+#         response.raise_for_status()  # Raise exception for non-200 responses
+#         data = response.text
 
-        # Create a temporary file to store the data
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_file:
-            temp_file.write(data.encode())
-            temp_file_path = temp_file.name
+#         # Create a temporary file to store the data
+#         with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_file:
+#             temp_file.write(data.encode())
+#             temp_file_path = temp_file.name
 
-        # Load data into Snowflake
-        # Specify your Snowflake connection ID and other parameters
-        snowflake_conn_id = 'snowflake_conn'
-        table_name = 'airflow_tasks'
-        schema_name = 'exusia_schema'
+#         # Load data into Snowflake
+#         # Specify your Snowflake connection ID and other parameters
+#         snowflake_conn_id = 'snowflake_conn'
+#         table_name = 'airflow_tasks'
+#         schema_name = 'exusia_schema'
 
-        load_query = f"""
-        COPY INTO {schema_name}.{table_name}
-        FROM '{temp_file_path}'
-        FILE_FORMAT = (TYPE = CSV)
-        """
+#         load_query = f"""
+#         COPY INTO {schema_name}.{table_name}
+#         FROM '{temp_file_path}'
+#         FILE_FORMAT = (TYPE = CSV)
+#         """
         
-        # Execute the query using the SnowflakeHook
-        snowflake_hook = SnowflakeHook(snowflake_conn_id=snowflake_conn_id)
-        snowflake_hook.run(load_query)
+#         # Execute the query using the SnowflakeHook
+#         snowflake_hook = SnowflakeHook(snowflake_conn_id=snowflake_conn_id)
+#         snowflake_hook.run(load_query)
         
-        logging.info("Data loaded into Snowflake successfully.")
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to fetch data from URL: {e}")
+#         logging.info("Data loaded into Snowflake successfully.")
+#     except requests.exceptions.RequestException as e:
+#         logging.error(f"Failed to fetch data from URL: {e}")
 
-load_data_task2 = PythonOperator(
-    task_id='load_data_task',
-    python_callable=read_data_and_load_to_snowflake,
-    provide_context=True,
-    dag=dag,
-)
+# load_data_task2 = PythonOperator(
+#     task_id='load_data_task',
+#     python_callable=read_data_and_load_to_snowflake,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-check_env_task1 >> load_data_task2
+# check_env_task1 >> load_data_task2
 
 
 # from airflow import DAG
@@ -367,34 +367,34 @@ check_env_task1 >> load_data_task2
 # check_env_task >> load_data_task
 
 
-# from airflow import DAG
-# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-# from datetime import datetime
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from datetime import datetime
 
-# default_args = {
-#     'owner': 'airflow',
-#     'start_date': datetime(2023, 1, 1),
-#     'retries': 1,
-# }
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+}
 
-# dag = DAG(
-#     'harsha_dag',  
-#     default_args=default_args,
-#     schedule_interval='@once',
-#     catchup=False,
-# )
+dag = DAG(
+    'harsha_dag',  
+    default_args=default_args,
+    schedule_interval='@once',
+    catchup=False,
+)
 
-# sql_query = """
-# SELECT * FROM patients WHERE status = 'Recovered'
-# """
+sql_query = """
+SELECT * FROM patients WHERE status = 'Recovered'
+"""
 
-# snowflake_task = SnowflakeOperator(
-#     task_id='execute_snowflake_query',
-#     sql=sql_query,
-#     snowflake_conn_id='snowflake_conn',
-#     autocommit=True,
-#     dag=dag,
-# )
+snowflake_task = SnowflakeOperator(
+    task_id='execute_snowflake_query',
+    sql=sql_query,
+    snowflake_conn_id='snowflake_conn',
+    autocommit=True,
+    dag=dag,
+)
 
 
 
