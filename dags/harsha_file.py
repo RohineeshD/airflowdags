571f65305329ef8475f49f9fabb23a63ed4e118e
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-24 17:51:05 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -16,7 +16,7 @@ dag = DAG(
 )
 
 sql_query = """
-"SELECT * FROM patients WHERE status = 'Recovered'
+SELECT * FROM patients WHERE status = 'Recovered'
 """
 
 snowflake_task = SnowflakeOperator(
@@ -26,3 +26,34 @@ snowflake_task = SnowflakeOperator(
     autocommit=True,
     dag=dag,
 )
+
+
+
+# from airflow import DAG
+# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+# from datetime import datetime
+
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2023, 1, 1),
+#     'retries': 1,
+# }
+
+# dag = DAG(
+#     'harsha_dag',  
+#     default_args=default_args,
+#     schedule_interval='@once',
+#     catchup=False,
+# )
+
+# sql_query = """
+# "SELECT * FROM patients WHERE status = 'Recovered'
+# """
+
+# snowflake_task = SnowflakeOperator(
+#     task_id='execute_snowflake_query',
+#     sql=sql_query,
+#     snowflake_conn_id='snowflake_conn',
+#     autocommit=True,
+#     dag=dag,
+# )
