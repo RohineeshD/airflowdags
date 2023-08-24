be0fd3499fe295edfc087a2f4cf91bb9b9332ab7
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-24 22:18:15 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -1,83 +1,71 @@
 from airflow import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.providers.snowflake.transfers.local_to_snowflake import LocalFilesystemToSnowflakeOperator
-from airflow.providers.http.sensors.http_sensor import HttpSensor
-from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
 import pandas as pd
 import requests
 import tempfile
-import os
 
 default_args = {
     'owner': 'airflow',
-    'start_date': days_ago(1),  # Set the start date
-    'catchup': False,
-    'provide_context': True,
+    'start_date': datetime(2023, 8, 25),
+    'retries': 1,
 }
 
 dag = DAG(
-    'airline_safety_dag_harsha',
+    'airline_safety_dag',
     default_args=default_args,
     schedule_interval=None,  # Set to None for manual triggering
-    max_active_runs=1,
-    concurrency=1,
-    tags=['example'],
+    catchup=False,
 )
 
-# Task 1: Check environment variable
 def check_env_variable(**kwargs):
-    env_variable_value = os.environ.get('MY_ENV_VARIABLE')  # Replace with your actual env variable name
-    if env_variable_value == 'true':
+    if kwargs['dag_run'].conf.get('load_data'):
         return 'load_data_task'
-    else:
-        return 'task_end'
 
-task_1 = PythonOperator(
-    task_id='check_env_variable',
+check_env_task = PythonOperator(
+    task_id='check_env_task',
     python_callable=check_env_variable,
     provide_context=True,
     dag=dag,
 )
 
-# Task 2: Read data from URL and load into Snowflake
 def read_data_and_load_to_snowflake(**kwargs):
     url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv"
     response = requests.get(url)
     data = response.text
     
-    # Save data to a temporary CSV file
-    with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
-        temp_file.write(data)
+    # Create a temporary file to store the data
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as temp_file:
+        temp_file.write(data.encode())
         temp_file_path = temp_file.name
+
+    # Load data into Snowflake using SnowflakeOperator
+    sql_query = f"""
+    COPY INTO airflow_tasks
+    FROM '{temp_file_path}'
+    FILE_FORMAT = (TYPE = CSV)
+    """
     
-    # Load data into Snowflake
-    snowflake_task = LocalFilesystemToSnowflakeOperator(
+    snowflake_task = SnowflakeOperator(
         task_id='load_data_into_snowflake',
-        schema='exusia_schema',
-        table='airflow_tasks',
-        filepaths=[temp_file_path],
+        sql=sql_query,
         snowflake_conn_id='snowflake_conn',  # Specify your Snowflake connection ID
+        autocommit=True,
         dag=dag,
     )
     
     snowflake_task.execute(context=kwargs)
 
-task_2 = PythonOperator(
+load_data_task = PythonOperator(
     task_id='load_data_task',
     python_callable=read_data_and_load_to_snowflake,
     provide_context=True,
     dag=dag,
 )
 
-# Task 3: Print process completed
-task_end = PythonOperator(
-    task_id='task_end',
-    python_callable=lambda: print('Process completed'),
-    dag=dag,
-)
-
-# Define task dependencies
-task_1 >> [task_2, task_end]
+check_env_task >> load_data_task
 
 
 # from airflow import DAG
