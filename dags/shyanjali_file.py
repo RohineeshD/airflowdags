10ffaee07eceb5a2cd2227c5d55fe85cea179002
Shyanjali4 <141653202+Shyanjali4@users.noreply.github.com>
2023-08-24 13:44:52 +0530

    Update shyanjali_file.py

--- dags/shyanjali_file.py
+++ dags/shyanjali_file.py
@@ -1,19 +1,50 @@
-from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from datetime import datetime
 
-def print_hello():
-    return 'Hello world from first Airflow DAG!'
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+}
 
-dag = DAG('shyanjali_dag',
-          schedule_interval='@once',
-          start_date=datetime(2022, 11, 12), catchup=False)
+dag = DAG(
+    'shyanjali_dags',  # Change the dag_id to a unique name
+    default_args=default_args,
+    schedule_interval='@once',
+    catchup=False,
+)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+sql_query = """
+SELECT SUM(id) AS total_id_sum
+FROM table1
+"""
 
-hello_operator
+snowflake_task = SnowflakeOperator(
+    task_id='execute_snowflake_query',
+    sql=sql_query,
+    snowflake_conn_id='snow_conn',
+    autocommit=True,
+    dag=dag,
+)
 
+# -----------------------------
+# from datetime import datetime
+# from airflow import DAG
+# from airflow.operators.dummy_operator import DummyOperator
+# from airflow.operators.python_operator import PythonOperator
+
+# def print_hello():
+#     return 'Hello world from first Airflow DAG!'
+
+# dag = DAG('shyanjali_dag',
+#           schedule_interval='@once',
+#           start_date=datetime(2022, 11, 12), catchup=False)
+
+# hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+
+# hello_operator
+# ---------------------------------
 # # Step 1: Importing Modules
 # # To initiate the DAG Object
 # from airflow import DAG
