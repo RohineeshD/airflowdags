972af6b23c62cef7e4d79c44743cbb93ffb0332f
jcharishma <141810544+jcharishma@users.noreply.github.com>
2023-08-24 13:51:31 +0530

    Update charishma_file.py

--- dags/charishma_file.py
+++ dags/charishma_file.py
@@ -1,32 +1,65 @@
 from airflow import DAG
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.operators import PythonOperator
 from datetime import datetime
 
+
+
+# Step 2: Initiating the default_args
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
+    'start_date': datetime(2022, 11, 12),
 }
 
+# Define a Python function to be executed by the PythonOperator
+def print_hello():
+    print("Hello from the PythonOperator!")
+
+# Step 3: Creating DAG Object
 dag = DAG(
-    'charishma_dags',  # Change the dag_id to a unique name
+    dag_id='charishma_dag',
     default_args=default_args,
-    schedule_interval=None,
+    schedule_interval='@once',  
     catchup=False,
 )
 
-sql_query = """
-SELECT SUM(id) AS total_id_sum
-FROM table1
-"""
-
-snowflake_task = SnowflakeOperator(
-    task_id='execute_snowflake_query',
-    sql=sql_query,
-    snowflake_conn_id='snow_conn',
-    autocommit=True,
+# Step 4: Creating task
+# Create a PythonOperator that will run the print_hello function
+task = PythonOperator(
+    task_id='print_hello_task',
+    python_callable=print_hello,
     dag=dag,
 )
+
+
+# from airflow import DAG
+# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+# from datetime import datetime
+
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2023, 1, 1),
+#     'retries': 1,
+# }
+
+# dag = DAG(
+#     'charishma_dags',  # Change the dag_id to a unique name
+#     default_args=default_args,
+#     schedule_interval=None,
+#     catchup=False,
+# )
+
+# sql_query = """
+# SELECT SUM(id) AS total_id_sum
+# FROM table1
+# """
+
+# snowflake_task = SnowflakeOperator(
+#     task_id='execute_snowflake_query',
+#     sql=sql_query,
+#     snowflake_conn_id='snow_conn',
+#     autocommit=True,
+#     dag=dag,
+# )
 
 
 
