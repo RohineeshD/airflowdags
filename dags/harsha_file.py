a2abc081d3c8c286cd0423ad7453e11bd49a4173
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-24 22:10:30 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -1,31 +1,113 @@
 from airflow import DAG
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.snowflake.transfers.local_to_snowflake import LocalFilesystemToSnowflakeOperator
+from airflow.providers.http.sensors.http_sensor import HttpSensor
+from airflow.utils.dates import days_ago
+import pandas as pd
+import requests
+import tempfile
+import os
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
+    'start_date': days_ago(1),  # Set the start date
+    'catchup': False,
+    'provide_context': True,
 }
 
 dag = DAG(
-    'harsha_dag',  
+    'airline_safety_dag_harsha',
     default_args=default_args,
-    schedule_interval='@once',
-    catchup=False,
+    schedule_interval=None,  # Set to None for manual triggering
+    max_active_runs=1,
+    concurrency=1,
+    tags=['example'],
 )
 
-sql_query = """
-SELECT * FROM patients WHERE status = 'Recovered'
-"""
+# Task 1: Check environment variable
+def check_env_variable(**kwargs):
+    env_variable_value = os.environ.get('MY_ENV_VARIABLE')  # Replace with your actual env variable name
+    if env_variable_value == 'true':
+        return 'load_data_task'
+    else:
+        return 'task_end'
 
-snowflake_task = SnowflakeOperator(
-    task_id='execute_snowflake_query',
-    sql=sql_query,
-    snowflake_conn_id='snowflake_conn',
-    autocommit=True,
+task_1 = PythonOperator(
+    task_id='check_env_variable',
+    python_callable=check_env_variable,
+    provide_context=True,
     dag=dag,
 )
+
+# Task 2: Read data from URL and load into Snowflake
+def read_data_and_load_to_snowflake(**kwargs):
+    url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv"
+    response = requests.get(url)
+    data = response.text
+    
+    # Save data to a temporary CSV file
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+        temp_file.write(data)
+        temp_file_path = temp_file.name
+    
+    # Load data into Snowflake
+    snowflake_task = LocalFilesystemToSnowflakeOperator(
+        task_id='load_data_into_snowflake',
+        schema='exusia_schema',
+        table='airflow_tasks',
+        filepaths=[temp_file_path],
+        snowflake_conn_id='snowflake_conn',  # Specify your Snowflake connection ID
+        dag=dag,
+    )
+    
+    snowflake_task.execute(context=kwargs)
+
+task_2 = PythonOperator(
+    task_id='load_data_task',
+    python_callable=read_data_and_load_to_snowflake,
+    provide_context=True,
+    dag=dag,
+)
+
+# Task 3: Print process completed
+task_end = PythonOperator(
+    task_id='task_end',
+    python_callable=lambda: print('Process completed'),
+    dag=dag,
+)
+
+# Define task dependencies
+task_1 >> [task_2, task_end]
+
+
+# from airflow import DAG
+# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+# from datetime import datetime
+
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2023, 1, 1),
+#     'retries': 1,
+# }
+
+# dag = DAG(
+#     'harsha_dag',  
+#     default_args=default_args,
+#     schedule_interval='@once',
+#     catchup=False,
+# )
+
+# sql_query = """
+# SELECT * FROM patients WHERE status = 'Recovered'
+# """
+
+# snowflake_task = SnowflakeOperator(
+#     task_id='execute_snowflake_query',
+#     sql=sql_query,
+#     snowflake_conn_id='snowflake_conn',
+#     autocommit=True,
+#     dag=dag,
+# )
 
 
 
