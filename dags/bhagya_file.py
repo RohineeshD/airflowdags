1efc59f31d3b75d1ab18e9edd7a3ea878618eb8f
bhagyanadar <141400166+bhagyanadar@users.noreply.github.com>
2023-08-25 16:09:33 +0530

    Update bhagya_file.py

--- dags/bhagya_file.py
+++ dags/bhagya_file.py
@@ -3,12 +3,12 @@
 from io import StringIO
 from airflow import DAG
 import os
+import requests
 # Importing datetime and timedelta modules for scheduling the DAGs
 from datetime import timedelta, datetime
 # Importing operators 
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
