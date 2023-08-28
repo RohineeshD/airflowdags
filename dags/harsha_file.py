1bd0609c4c34cc9cb4c55dc46a778248791d7df9
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-28 11:50:05 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -1,6 +1,6 @@
- from airflow import DAG
- from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
- from datetime import datetime
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from datetime import datetime
 
 default_args = {
      'owner': 'airflow',
