c88acb643e08ba89095860bc246c8842f9a24900
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-08-29 23:41:57 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -21,7 +21,7 @@ dag = DAG(
 
 def check_env_variable(**kwargs):
     harsha_air_env = os.environ.get('harsha_air_env', '').lower()
-    print(f"Value of harsh_air_env: {harsah_air_env}")
+    print(f"Value of harsh_air_env: {harsha_air_env}")
     
     if harsha_air_env == 'true':
         print("Environment variable is 'true'. Proceeding with the DAG.")
