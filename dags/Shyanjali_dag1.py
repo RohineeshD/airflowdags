655ced030b4335d5ae434c34c6128a73acf91dd4
Shyanjali4 <141653202+Shyanjali4@users.noreply.github.com>
2023-08-31 03:35:22 +0530

    Update Shyanjali_dag1.py

--- dags/Shyanjali_dag1.py
+++ dags/Shyanjali_dag1.py
@@ -2,33 +2,54 @@ from airflow import DAG
 from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
+import logging
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
 
-dag = DAG(
-    'load_data_to_staging',
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+default_args = dict(
+    start_date= datetime(2021, 1, 1),
+    owner="airflow",
+    retries=1,
+)
+
+dag_args = dict(
+    dag_id="Shyanjali_dag1",
+    schedule_interval='@once',
     default_args=default_args,
-    schedule_interval=None,  # Set the schedule interval as needed
     catchup=False,
 )
 
-load_to_staging = S3ToSnowflakeOperator(
-    task_id='load_data_to_staging',
-    schema='PUBLIC',
-    table='<your_staging_table>',
-    stage='<your_snowflake_stage>',
-    file_format='<your_snowflake_file_format>',
-    s3_bucket='<your_s3_bucket>',
-    s3_key='<your_s3_key>',
-    aws_conn_id='<your_aws_connection_id>',
-    snowflake_conn_id='<your_snowflake_connection_id>',
-    dag=dag,
+def fetch_csv_and_upload(**kwargs):
+    url = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
+    response = requests.get(url)
+    data = response.text
+    df = pd.read_csv(StringIO(data))
+    # Upload DataFrame to Snowflake
+    snowflake_hook = SnowflakeHook(snowflake_conn_id='snowflake_li')
+    # Replace with your Snowflake schema and table name
+    schema = 'PUBLIC'
+    table_name = 'STAGING_TABLE'
+    connection = snowflake_hook.get_conn()
+    snowflake_hook.insert_rows(table_name, df.values.tolist())
+    connection.close()
+
+
+with DAG(**dag_args) as dag:
+    # first task declaration
+    fetch_and_upload = PythonOperator(
+        task_id='fetch_and_upload',
+        python_callable=fetch_csv_and_upload,
+        provide_context=True,
+        op_kwargs={},# This is required to pass context to the function
+    )
+
+trigger_dag2 = TriggerDagRunOperator(
+    task_id='trigger_dag2',
+    trigger_dag_id="Shyanjali_dag2",
+    dag=Shyanjali_dag1,
 )
 
-load_to_staging
+fetch_and_upload >> trigger_dag2
