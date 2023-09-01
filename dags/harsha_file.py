54641c49451233b7e994a436bcd2354b98eb8c6f
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-01 16:54:34 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -26,7 +26,7 @@ def read_and_load_to_snowflake(**kwargs):
     if response.status_code == 200:
         data = response.text
         lines = data.strip().split('\n')[1:]
-        snowflake_hook = SnowflakeHook(snowflake_conn_id="s_h_connection")
+        snowflake_hook = SnowflakeHook(snowflake_conn_id='s_h_connection')
         
         for line in lines:
             values = line.split(',')
