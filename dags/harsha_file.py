00772b0abf44d9fe04e346954e140ae89e421170
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-04 18:23:31 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -26,12 +26,12 @@ def check_environment_variable():
     # variable_value = 'harsha_air_env'
     # variable_value = Variable.get('harsha_air_env')
     # return variable_value == "True"
-    Variable.set("my_boolean_variable", True)
-    bool = Variable.get('my_boolean_variable')
-    if bool :
-        print( bool )
-        return True
-    if Variable.get('harsha_air_env').lower() == 'true':
+    # Variable.set("my_boolean_variable", True)
+    # bool = Variable.get('my_boolean_variable')
+    # if bool :
+    #     print( bool )
+    #     return True
+    if Variable.get('harsha_air_env').lower() == 'True':
         return 'load_data_to_snowflake'
     else:
         #stop dag
