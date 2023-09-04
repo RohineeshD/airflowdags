ad00cff5e1815e1bc31ed911d27f1359e37c25b8
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-04 19:23:59 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -45,7 +45,7 @@ def check_environment_variable():
     variable_value = Variable.get('harsha_air_env')
     print(f'harsha_air_env = {variable_value}')
     
-    if variable_value == "True":
+    if variable_value == "TRUE":
         return True
     else:
         return False
