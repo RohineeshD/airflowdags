354da965e91b682b8a1df8f81abc14f772061ef5
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-04 13:02:02 +0530

    Update harsha_file.py

--- dags/harsha_file.py
+++ dags/harsha_file.py
@@ -1,140 +1,140 @@
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.dates import days_ago
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from io import StringIO
-import pandas as pd
-import requests
-import logging
+# from datetime import datetime
+# from airflow import DAG
+# from airflow.operators.python_operator import PythonOperator
+# from airflow.operators.dummy_operator import DummyOperator
+# from airflow.utils.dates import days_ago
+# from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+# from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+# from io import StringIO
+# import pandas as pd
+# import requests
+# import logging
 
-default_args = {
-    'owner': 'airflow',
-    'start_date': days_ago(1),
-    'catchup': False,
-    'provide_context': True,
-}
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': days_ago(1),
+#     'catchup': False,
+#     'provide_context': True,
+# }
 
-dag = DAG(
-    'dag_1_harsha',
-    default_args=default_args,
-    schedule_interval=None,
-)
+# dag = DAG(
+#     'dag_1_harsha',
+#     default_args=default_args,
+#     schedule_interval=None,
+# )
 
-# Define  Snowflake connection credentials
-SNOWFLAKE_CONN_ID = 'snowflake_conn'  
-SNOWFLAKE_SCHEMA = 'exusia_schema'  
-STAGING_TABLE = 'harsha_harsha'  
-MAIN_TABLE = 'main_harsha'  
+# # Define  Snowflake connection credentials
+# SNOWFLAKE_CONN_ID = 'snowflake_conn'  
+# SNOWFLAKE_SCHEMA = 'exusia_schema'  
+# STAGING_TABLE = 'harsha_harsha'  
+# MAIN_TABLE = 'main_harsha'  
 
-# Snowflake connection setup
-def get_snowflake_hook(conn_id):
-    return SnowflakeHook(snowflake_conn_id=conn_id)
+# # Snowflake connection setup
+# def get_snowflake_hook(conn_id):
+#     return SnowflakeHook(snowflake_conn_id=conn_id)
 
-# Function to read data from the URL
-def read_data_from_url(**kwargs):
-    try:
-        url = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
-        response = requests.get(url)
-        data = response.text
-        df = pd.read_csv(StringIO(data))
+# # Function to read data from the URL
+# def read_data_from_url(**kwargs):
+#     try:
+#         url = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
+#         response = requests.get(url)
+#         data = response.text
+#         df = pd.read_csv(StringIO(data))
         
-        # Convert the DataFrame to a CSV string
-        csv_data = df.to_csv(index=False)
+#         # Convert the DataFrame to a CSV string
+#         csv_data = df.to_csv(index=False)
         
-        kwargs['ti'].xcom_push(key='data_frame_csv', value=csv_data)  # Push the CSV data to XCom
-        return True
-    except Exception as e:
-        print(f"An error occurred while reading data: {str(e)}")
-        return False
+#         kwargs['ti'].xcom_push(key='data_frame_csv', value=csv_data)  # Push the CSV data to XCom
+#         return True
+#     except Exception as e:
+#         print(f"An error occurred while reading data: {str(e)}")
+#         return False
 
-# Function to load data into Snowflake
-def load_data_into_snowflake(**kwargs):
-    try:
-        # Retrieve the CSV data from XCom
-        csv_data = kwargs['ti'].xcom_pull(key='data_frame_csv', task_ids='read_data_from_url')
+# # Function to load data into Snowflake
+# def load_data_into_snowflake(**kwargs):
+#     try:
+#         # Retrieve the CSV data from XCom
+#         csv_data = kwargs['ti'].xcom_pull(key='data_frame_csv', task_ids='read_data_from_url')
         
-        # Convert the CSV data to a DataFrame
-        df = pd.read_csv(StringIO(csv_data))
+#         # Convert the CSV data to a DataFrame
+#         df = pd.read_csv(StringIO(csv_data))
         
-        # Upload DataFrame to Snowflake
-        snowflake_hook = get_snowflake_hook(SNOWFLAKE_CONN_ID)
-        connection = snowflake_hook.get_conn()
-        snowflake_hook.insert_rows(f'{SNOWFLAKE_SCHEMA}.{STAGING_TABLE}', df.values.tolist())
-        connection.close()
-        return True
-    except Exception as e:
-        print(f"An error occurred while loading data into Snowflake: {str(e)}")
-        return False
+#         # Upload DataFrame to Snowflake
+#         snowflake_hook = get_snowflake_hook(SNOWFLAKE_CONN_ID)
+#         connection = snowflake_hook.get_conn()
+#         snowflake_hook.insert_rows(f'{SNOWFLAKE_SCHEMA}.{STAGING_TABLE}', df.values.tolist())
+#         connection.close()
+#         return True
+#     except Exception as e:
+#         print(f"An error occurred while loading data into Snowflake: {str(e)}")
+#         return False
 
-# Function to check the data
-def check_data(**kwargs):
-    try:
-        # Retrieve the CSV data from XCom
-        csv_data = kwargs['ti'].xcom_pull(key='data_frame_csv', task_ids='read_data_from_url')
+# # Function to check the data
+# def check_data(**kwargs):
+#     try:
+#         # Retrieve the CSV data from XCom
+#         csv_data = kwargs['ti'].xcom_pull(key='data_frame_csv', task_ids='read_data_from_url')
         
-        # Convert the CSV data to a DataFrame
-        df = pd.read_csv(StringIO(csv_data))
+#         # Convert the CSV data to a DataFrame
+#         df = pd.read_csv(StringIO(csv_data))
 
-        # Check if there are any rows in the DataFrame
-        # if not df.empty:
-        #     logging.info("Data check passed. Rows found in the DataFrame.")
+#         # Check if there are any rows in the DataFrame
+#         # if not df.empty:
+#         #     logging.info("Data check passed. Rows found in the DataFrame.")
 
-            # You can also execute a SQL query to check the table in Snowflake
-        snowflake_hook = get_snowflake_hook(SNOWFLAKE_CONN_ID)
-        connection = snowflake_hook.get_conn()
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT * FROM {SNOWFLAKE_SCHEMA}.{STAGING_TABLE}")
-        result = cursor.fetchall()
-        cursor.close()
-        connection.close()
+#             # You can also execute a SQL query to check the table in Snowflake
+#         snowflake_hook = get_snowflake_hook(SNOWFLAKE_CONN_ID)
+#         connection = snowflake_hook.get_conn()
+#         cursor = connection.cursor()
+#         cursor.execute(f"SELECT * FROM {SNOWFLAKE_SCHEMA}.{STAGING_TABLE}")
+#         result = cursor.fetchall()
+#         cursor.close()
+#         connection.close()
 
-            # if result:
-            #     logging.info("Data check passed. Rows found in the Snowflake table.")
-            # else:
-            #     logging.warning("Data check failed. No rows found in the Snowflake table.")
-        # else:
-        #     logging.warning("Data check failed. DataFrame is empty.")
+#             # if result:
+#             #     logging.info("Data check passed. Rows found in the Snowflake table.")
+#             # else:
+#             #     logging.warning("Data check failed. No rows found in the Snowflake table.")
+#         # else:
+#         #     logging.warning("Data check failed. DataFrame is empty.")
         
-        # return True
-    except Exception as e:
-        print(f"An error occurred while checking data: {str(e)}")
-        return False
+#         # return True
+#     except Exception as e:
+#         print(f"An error occurred while checking data: {str(e)}")
+#         return False
 
-# Task 1: Read data from the URL
-read_data_task = PythonOperator(
-    task_id='read_data_from_url',
-    python_callable=read_data_from_url,
-    provide_context=True,
-    dag=dag,
-)
+# # Task 1: Read data from the URL
+# read_data_task = PythonOperator(
+#     task_id='read_data_from_url',
+#     python_callable=read_data_from_url,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-# Task 2: Load data into Snowflake
-load_data_task = PythonOperator(
-    task_id='load_data_into_snowflake',
-    python_callable=load_data_into_snowflake,
-    provide_context=True,
-    dag=dag,
-)
+# # Task 2: Load data into Snowflake
+# load_data_task = PythonOperator(
+#     task_id='load_data_into_snowflake',
+#     python_callable=load_data_into_snowflake,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-# Task 3: Check the data
-check_data_task = PythonOperator(
-    task_id='check_data',
-    python_callable=check_data,
-    provide_context=True,
-    dag=dag,
-)
+# # Task 3: Check the data
+# check_data_task = PythonOperator(
+#     task_id='check_data',
+#     python_callable=check_data,
+#     provide_context=True,
+#     dag=dag,
+# )
 
-trigger_dag_2 = TriggerDagRunOperator(
-    task_id='trigger_dag_2',
-    trigger_dag_id="dag_2_harsha",
-    dag=dag,
-)
+# trigger_dag_2 = TriggerDagRunOperator(
+#     task_id='trigger_dag_2',
+#     trigger_dag_id="dag_2_harsha",
+#     dag=dag,
+# )
 
 
-read_data_task >> load_data_task >> check_data_task >> trigger_dag_2 
+# read_data_task >> load_data_task >> check_data_task >> trigger_dag_2 
 
 
 # from datetime import datetime
