e6514e66bdecd8c6220c339e2573456a0bde8501
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-05 23:52:53 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -33,41 +33,33 @@ def load_csv_to_snowflake():
         conn = snowflake_hook.get_conn()
         cursor = conn.cursor()
 
-        # Create a Snowflake internal stage for the CSV file
-        stage_name = 'csv_stage'
-        create_stage_sql = f'''
-        CREATE OR REPLACE STAGE {stage_name}
-        FILE_FORMAT = (
-            TYPE = 'CSV'
-            SKIP_HEADER = 1
-        );
-        '''
-        cursor.execute(create_stage_sql)
-
         # Download the CSV file to a local directory
         response = requests.get(csv_url)
         local_file_path = '/tmp/customers-100000.csv'
         with open(local_file_path, 'wb') as file:
             file.write(response.content)
 
-        # Upload the CSV file to the Snowflake internal stage
-        put_sql = f'''
-        PUT 'file://{local_file_path}' @{stage_name}
-        '''
-        cursor.execute(put_sql)
-
-        # Snowflake COPY INTO command using the internal stage
+        # Snowflake COPY INTO command using the local file path
         copy_into_sql = f'''
         COPY INTO {snowflake_table}
-        FROM @{stage_name}
+        FROM '{local_file_path}'
+        FILE_FORMAT = (
+            TYPE = 'CSV'
+            SKIP_HEADER = 1
+        );
         '''
-        cursor.execute(copy_into_sql)
 
-        # Drop the Snowflake internal stage after loading
-        drop_stage_sql = f'''
-        DROP STAGE IF EXISTS {stage_name}
-        '''
-        cursor.execute(drop_stage_sql)
+        # Execute the COPY INTO command using SnowflakeOperator
+        copy_to_snowflake_task = SnowflakeOperator(
+            task_id='copy_to_snowflake_task',
+            sql=copy_into_sql,
+            snowflake_conn_id=snowflake_conn_id,
+            autocommit=True,
+            dag=dag
+        )
+
+        # Trigger the SnowflakeOperator task
+        copy_to_snowflake_task.execute(context=None)
 
         cursor.close()
         conn.close()
