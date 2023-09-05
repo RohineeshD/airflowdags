615fa5de7115880cb660c7c6b290d99e32f78c55
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-05 16:52:10 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
@@ -13,7 +12,7 @@ default_args = {
 }
 
 dag = DAG(
-     'harsha_dag2',
+     'harsh_dag',
      default_args=default_args,
      schedule_interval=None,  
      catchup=False,
@@ -28,13 +27,13 @@ def load_data():
 
         # Truncate the main_table before loading data
         truncate_query = """
-        TRUNCATE TABLE harsha_harsha;
+        TRUNCATE TABLE main_table;
         """
         cursor.execute(truncate_query)
 
         # Insert data from stage_table into main_table
         sql_query = """
-        INSERT INTO harsha_harsha (Country, Region)
+        INSERT INTO main_table (Country, Region)
         SELECT Country, Region
         FROM stage_table;
         """
@@ -47,30 +46,6 @@ def load_data():
     except Exception as e:
         print("Data loading failed -", str(e))
         return False
-
-
-# # Task 1: Load Data from Staging Table to Main
-# def load_data():
-#      try:
-#          snowflake_hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
-#          conn = snowflake_hook.get_conn()
-#          cursor = conn.cursor()
-
-        
-#          sql_query = """
-#          INSERT INTO main_table (Country, Region)
-#          SELECT Country, Region
-#          FROM stage_table;
-#          """
-
-#          cursor.execute(sql_query)
-#          cursor.close()
-#          conn.close()
-#          print("Data loaded successfully")
-#          return True
-#      except Exception as e:
-#          print("Data loading failed -", str(e))
-#          return False
 
 # # Task 2: Check if Load is Successful
 def check_load_status():
@@ -126,3 +101,132 @@ print_status_task = PythonOperator(
 
 
 load_data_task >> check_load_status_task >> print_status_task
+
+
+# from datetime import datetime
+# from airflow import DAG
+# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+# from airflow.operators.python_operator import PythonOperator
+# from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+# # # Define the DAG
+# default_args = {
+#      'owner': 'airflow',
+#      'start_date': datetime(2023, 9, 1),
+#      'retries': 1,
+# }
+
+# dag = DAG(
+#      'harsha_dag2',
+#      default_args=default_args,
+#      schedule_interval=None,  
+#      catchup=False,
+    
+# )
+
+# def load_data():
+#     try:
+#         snowflake_hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+#         conn = snowflake_hook.get_conn()
+#         cursor = conn.cursor()
+
+#         # Truncate the main_table before loading data
+#         truncate_query = """
+#         TRUNCATE TABLE main_table;
+#         """
+#         cursor.execute(truncate_query)
+
+#         # Insert data from stage_table into main_table
+#         sql_query = """
+#         INSERT INTO main_table (Country, Region)
+#         SELECT Country, Region
+#         FROM stage_table;
+#         """
+#         cursor.execute(sql_query)
+
+#         cursor.close()
+#         conn.close()
+#         print("Data loaded successfully")
+#         return True
+#     except Exception as e:
+#         print("Data loading failed -", str(e))
+#         return False
+
+
+# # # Task 1: Load Data from Staging Table to Main
+# # def load_data():
+# #      try:
+# #          snowflake_hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+# #          conn = snowflake_hook.get_conn()
+# #          cursor = conn.cursor()
+
+        
+# #          sql_query = """
+# #          INSERT INTO main_table (Country, Region)
+# #          SELECT Country, Region
+# #          FROM stage_table;
+# #          """
+
+# #          cursor.execute(sql_query)
+# #          cursor.close()
+# #          conn.close()
+# #          print("Data loaded successfully")
+# #          return True
+# #      except Exception as e:
+# #          print("Data loading failed -", str(e))
+# #          return False
+
+# # # Task 2: Check if Load is Successful
+# def check_load_status():
+#      try:
+#          snowflake_hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+#          conn = snowflake_hook.get_conn()
+#          cursor = conn.cursor()
+
+        
+#          sql_query = "SELECT * FROM main_table;"
+#          cursor.execute(sql_query)
+#          row = cursor.fetchall()
+
+        
+#          print("Load was successful")
+#          return True
+             
+#      except Exception as e:
+#          print("Load failed -", str(e))
+#          return False
+
+# # # Task 3: Print Success or Failure Status
+# def print_status(load_success):
+#      if load_success:
+#          print("Success")
+#      else:
+#          print("Failure")
+
+
+# # # Task 1: Load Data
+# load_data_task = PythonOperator(
+#      task_id='load_data',
+#      python_callable=load_data,
+#      dag=dag,
+# )
+
+# # # Task 2: Check Load Status
+# check_load_status_task = PythonOperator(
+#      task_id='check_load_status',
+#      python_callable=check_load_status,
+#      provide_context=True,
+#      dag=dag,
+# )
+
+# # # Task 3: Print Status
+# print_status_task = PythonOperator(
+#      task_id='print_status',
+#      python_callable=print_status,
+#      op_args=[check_load_status_task.output],
+#      provide_context=True,
+#      dag=dag,
+# )
+
+
+# load_data_task >> check_load_status_task >> print_status_task
