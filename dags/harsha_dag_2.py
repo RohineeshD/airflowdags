b4fd00f1a7245f718875c5910f4c52d19a59cb6f
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-06 22:02:04 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -1,115 +1,111 @@
-# from airflow import DAG
-# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-# from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
-# from airflow.operators.python_operator import PythonOperator
-# from airflow.utils.dates import days_ago
-# from datetime import datetime
-# import requests
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime
+import requests
 
-# # Define default_args for the DAG
-# default_args = {
-#     'owner': 'airflow',
-#     'start_date': days_ago(1),
-#     'schedule_interval': None,  
-#     'catchup': False
-# }
+# Define default_args for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'schedule_interval': None,  
+    'catchup': False
+}
 
-# dag = DAG(
-#     'load_snowflake',
-#     default_args=default_args,
-#     description='Load CSV data into Snowflake',
-#     catchup=False
-# )
+dag = DAG(
+    'load_snowflake',
+    default_args=default_args,
+    catchup=False
+)
 
-# # Define Snowflake connection ID from Airflow's Connection UI
-# snowflake_conn_id = 'air_conn'
+# Define Snowflake connection ID from Airflow's Connection UI
+snowflake_conn_id = 'air_conn'
 
-# def get_snowflake_hook(conn_id):
-#     return SnowflakeHook(snowflake_conn_id=conn_id)
+def get_snowflake_hook(conn_id):
+    return SnowflakeHook(snowflake_conn_id=conn_id)
 
-# # Define Snowflake target table
-# snowflake_table = 'bulk_table'
+# Define Snowflake target table
+snowflake_table = 'bulk_table'
 
-# # Define the CSV URL
-# csv_url = 'https://media.githubusercontent.com/media/datablist/sample-csv-files/main/files/customers/customers-100000.csv'
+# Define the CSV URL
+csv_url = 'https://media.githubusercontent.com/media/datablist/sample-csv-files/main/files/customers/customers-100000.csv'
 
-# # Function to load CSV data into Snowflake
-# def load_csv_to_snowflake():
-#     try:
-#         snowflake_hook = get_snowflake_hook(snowflake_conn_id)
+# Function to load CSV data into Snowflake
+def load_csv_to_snowflake():
+    try:
+        snowflake_hook = get_snowflake_hook(snowflake_conn_id)
 
-#         # Establish a Snowflake connection
-#         conn = snowflake_hook.get_conn()
-#         cursor = conn.cursor()
+        # Establish a Snowflake connection
+        conn = snowflake_hook.get_conn()
+        cursor = conn.cursor()
 
-#         # Create a Snowflake internal stage for the CSV file
-#         stage_name = 'csv_stage'
-#         create_stage_sql = f'''
-#         CREATE OR REPLACE STAGE {stage_name}
-#         FILE_FORMAT = (
-#             TYPE = 'CSV'
-#             SKIP_HEADER = 1
-#         );
-#         '''
-#         cursor.execute(create_stage_sql)
+        # Create a Snowflake internal stage for the CSV file
+        stage_name = 'csv_stage'
+        create_stage_sql = f'''
+        CREATE OR REPLACE STAGE {stage_name}
+        FILE_FORMAT = (
+            TYPE = 'CSV'
+            SKIP_HEADER = 1
+        );
+        '''
+        cursor.execute(create_stage_sql)
 
-#         # Download the CSV file to a local directory
-#         response = requests.get(csv_url)
-#         local_file_path = '/tmp/customers-100000.csv'
-#         with open(local_file_path, 'wb') as file:
-#             file.write(response.content)
+        # Download the CSV file to a local directory
+        response = requests.get(csv_url)
+        local_file_path = '/tmp/customers-100000.csv'
+        with open(local_file_path, 'wb') as file:
+            file.write(response.content)
 
-#         # Upload the CSV file to the Snowflake internal stage
-#         put_sql = f'''
-#         PUT 'file://{local_file_path}' @{stage_name}
-#         '''
-#         cursor.execute(put_sql)
+        # Upload the CSV file to the Snowflake internal stage
+        put_sql = f'''
+        PUT 'file://{local_file_path}' @{stage_name}
+        '''
+        cursor.execute(put_sql)
 
-#         # Snowflake COPY INTO command using the internal stage
-#         copy_into_sql = f'''
-#         COPY INTO {snowflake_table}
-#         FROM @{stage_name}
-#         FILE_FORMAT = (
-#             TYPE = 'CSV'
-#             SKIP_HEADER = 1
-#             FIELD_OPTIONALLY_ENCLOSED_BY = '',
-#             ERROR_ON_COLUMN_COUNT_MISMATCH = TRUE
-#             SKIP_BYTE_ORDER_MARK = TRUE
-#             STRIP_NULL_VALUES = FALSE
-#             SKIP_UTF8_BOM = TRUE
-#             ON_ERROR = 'CONTINUE'
-#         );
-#         '''
-#         cursor.execute(copy_into_sql)
+        # Snowflake COPY INTO command using the internal stage
+        copy_into_sql = f'''
+        COPY INTO {snowflake_table}
+        FROM @{stage_name}
+        FILE_FORMAT = (
+            TYPE = 'CSV'
+            SKIP_HEADER = 1
+            FIELD_OPTIONALLY_ENCLOSED_BY = '',
+            ERROR_ON_COLUMN_COUNT_MISMATCH = TRUE
+            SKIP_BYTE_ORDER_MARK = TRUE
+            STRIP_NULL_VALUES = FALSE
+            SKIP_UTF8_BOM = TRUE
+            ON_ERROR = 'CONTINUE'
+        );
+        '''
+        cursor.execute(copy_into_sql)
 
-#         # Drop the Snowflake internal stage after loading
-#         drop_stage_sql = f'''
-#         DROP STAGE IF EXISTS {stage_name}
-#         '''
-#         cursor.execute(drop_stage_sql)
+        # Drop the Snowflake internal stage after loading
+        drop_stage_sql = f'''
+        DROP STAGE IF EXISTS {stage_name}
+        '''
+        cursor.execute(drop_stage_sql)
 
-#         cursor.close()
-#         conn.close()
+        cursor.close()
+        conn.close()
 
-#         print("Data loaded successfully")
-#         return True
-#     except Exception as e:
-#         print("Data loading failed -", str(e))
-#         return False
+        print("Data loaded successfully")
+        return True
+    except Exception as e:
+        print("Data loading failed -", str(e))
+        return False
 
-# # Task to call the load_csv_to_snowflake function
-# load_csv_task = PythonOperator(
-#     task_id='load_csv_to_snowflake_task',
-#     python_callable=load_csv_to_snowflake,
-#     dag=dag
-# )
+# Task to call the load_csv_to_snowflake function
+load_csv_task = PythonOperator(
+    task_id='load_csv_to_snowflake_task',
+    python_callable=load_csv_to_snowflake,
+    dag=dag
+)
 
-# # Set task dependencies if needed
-# # load_csv_task.set_upstream(...)
-# # load_csv_task.set_downstream(...)
 
-# if __name__ == "__main__":
-#     dag.cli()
+if __name__ == "__main__":
+    dag.cli()
 
 
 # from airflow import DAG
@@ -1247,130 +1243,130 @@
 # load_data_task >> check_load_status_task >> print_status_task
 
 
-from datetime import datetime
-from airflow import DAG
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+# from datetime import datetime
+# from airflow import DAG
+# from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+# from airflow.operators.python_operator import PythonOperator
+# from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 
-# # Define the DAG
-default_args = {
-     'owner': 'airflow',
-     'start_date': datetime(2023, 9, 1),
-     'retries': 1,
-}
+# # # Define the DAG
+# default_args = {
+#      'owner': 'airflow',
+#      'start_date': datetime(2023, 9, 1),
+#      'retries': 1,
+# }
 
-dag = DAG(
-     'harsha_dag2',
-     default_args=default_args,
-     schedule_interval=None,  
-     catchup=False,
+# dag = DAG(
+#      'harsha_dag2',
+#      default_args=default_args,
+#      schedule_interval=None,  
+#      catchup=False,
     
-)
+# )
 
-def load_data():
-    try:
-        snowflake_hook = SnowflakeHook(snowflake_conn_id='air_conn')
-        conn = snowflake_hook.get_conn()
-        cursor = conn.cursor()
-
-        # Truncate the main_table before loading data
-        truncate_query = """
-        TRUNCATE TABLE main_table;
-        """
-        cursor.execute(truncate_query)
-
-        # Insert data from stage_table into main_table
-        sql_query = """
-        INSERT INTO main_table (Country, Region)
-        SELECT Country, Region
-        FROM stage_table;
-        """
-        cursor.execute(sql_query)
-
-        cursor.close()
-        conn.close()
-        print("Data loaded successfully")
-        return True
-    except Exception as e:
-        print("Data loading failed -", str(e))
-        return False
-
-
-# # Task 1: Load Data from Staging Table to Main
 # def load_data():
+#     try:
+#         snowflake_hook = SnowflakeHook(snowflake_conn_id='air_conn')
+#         conn = snowflake_hook.get_conn()
+#         cursor = conn.cursor()
+
+#         # Truncate the main_table before loading data
+#         truncate_query = """
+#         TRUNCATE TABLE main_table;
+#         """
+#         cursor.execute(truncate_query)
+
+#         # Insert data from stage_table into main_table
+#         sql_query = """
+#         INSERT INTO main_table (Country, Region)
+#         SELECT Country, Region
+#         FROM stage_table;
+#         """
+#         cursor.execute(sql_query)
+
+#         cursor.close()
+#         conn.close()
+#         print("Data loaded successfully")
+#         return True
+#     except Exception as e:
+#         print("Data loading failed -", str(e))
+#         return False
+
+
+# # # Task 1: Load Data from Staging Table to Main
+# # def load_data():
+# #      try:
+# #          snowflake_hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+# #          conn = snowflake_hook.get_conn()
+# #          cursor = conn.cursor()
+
+        
+# #          sql_query = """
+# #          INSERT INTO main_table (Country, Region)
+# #          SELECT Country, Region
+# #          FROM stage_table;
+# #          """
+
+# #          cursor.execute(sql_query)
+# #          cursor.close()
+# #          conn.close()
+# #          print("Data loaded successfully")
+# #          return True
+# #      except Exception as e:
+# #          print("Data loading failed -", str(e))
+# #          return False
+
+# # # Task 2: Check if Load is Successful
+# def check_load_status():
 #      try:
-#          snowflake_hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+#          snowflake_hook = SnowflakeHook(snowflake_conn_id='air_conn')
 #          conn = snowflake_hook.get_conn()
 #          cursor = conn.cursor()
 
         
-#          sql_query = """
-#          INSERT INTO main_table (Country, Region)
-#          SELECT Country, Region
-#          FROM stage_table;
-#          """
-
+#          sql_query = "SELECT * FROM main_table;"
 #          cursor.execute(sql_query)
-#          cursor.close()
-#          conn.close()
-#          print("Data loaded successfully")
+#          row = cursor.fetchall()
+
+        
+#          print("Load was successful")
 #          return True
+             
 #      except Exception as e:
-#          print("Data loading failed -", str(e))
+#          print("Load failed -", str(e))
 #          return False
 
-# # Task 2: Check if Load is Successful
-def check_load_status():
-     try:
-         snowflake_hook = SnowflakeHook(snowflake_conn_id='air_conn')
-         conn = snowflake_hook.get_conn()
-         cursor = conn.cursor()
-
-        
-         sql_query = "SELECT * FROM main_table;"
-         cursor.execute(sql_query)
-         row = cursor.fetchall()
-
-        
-         print("Load was successful")
-         return True
-             
-     except Exception as e:
-         print("Load failed -", str(e))
-         return False
-
-# # Task 3: Print Success or Failure Status
-def print_status(load_success):
-     if load_success:
-         print("Success")
-     else:
-         print("Failure")
+# # # Task 3: Print Success or Failure Status
+# def print_status(load_success):
+#      if load_success:
+#          print("Success")
+#      else:
+#          print("Failure")
 
 
-# # Task 1: Load Data
-load_data_task = PythonOperator(
-     task_id='load_data',
-     python_callable=load_data,
-     dag=dag,
-)
+# # # Task 1: Load Data
+# load_data_task = PythonOperator(
+#      task_id='load_data',
+#      python_callable=load_data,
+#      dag=dag,
+# )
 
-# # Task 2: Check Load Status
-check_load_status_task = PythonOperator(
-     task_id='check_load_status',
-     python_callable=check_load_status,
-     provide_context=True,
-     dag=dag,
-)
+# # # Task 2: Check Load Status
+# check_load_status_task = PythonOperator(
+#      task_id='check_load_status',
+#      python_callable=check_load_status,
+#      provide_context=True,
+#      dag=dag,
+# )
 
-# # Task 3: Print Status
-print_status_task = PythonOperator(
-     task_id='print_status',
-     python_callable=print_status,
-     op_args=[check_load_status_task.output],
-     provide_context=True,
-     dag=dag,
-)
+# # # Task 3: Print Status
+# print_status_task = PythonOperator(
+#      task_id='print_status',
+#      python_callable=print_status,
+#      op_args=[check_load_status_task.output],
+#      provide_context=True,
+#      dag=dag,
+# )
 
 
-load_data_task >> check_load_status_task >> print_status_task
+# load_data_task >> check_load_status_task >> print_status_task
