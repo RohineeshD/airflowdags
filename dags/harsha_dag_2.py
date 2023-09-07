5a24e747e9a1209ca7bf52133cb86ca07a0fb6ce
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-07 20:08:28 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -64,10 +64,10 @@ def insert_data_to_snowflake(table_name, snowflake_conn_id, csv_url):
                         
                     """
                     # Execute the query with parameter binding
-                    # snowflake_hook.run(query, values[1], values[2], values[3], values[4], values[5], values[6], values[8], values[9], values[10], values[11], values[12])
+                    snowflake_hook.run(query, values[1], values[2], values[3], values[4], values[5], values[6], values[8], values[9], values[10], values[11], values[12])
                     # cursor.execute(query, *values[1:13])
                     # Execute the query with parameter binding
-                    snowflake_hook.run(query, tuple(values[1:]))
+                    # snowflake_hook.run(query, tuple(values[1:]))
                 else:
                     print("Skipping row with insufficient columns.")
 
