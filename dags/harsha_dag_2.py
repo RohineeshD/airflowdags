e903bded94c5a6dff57b927cac02360773fe0fc8
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-07 20:25:19 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -69,8 +69,8 @@ def insert_data_to_snowflake(table_name, snowflake_conn_id, csv_url):
                     # Execute the query with parameter binding
                     # snowflake_hook.run(query, tuple(values[1:]))
                     snowflake_hook.run(query, parameters=(
-                            values[1], values[2], values[3], values[4], values[5], values[6],
-                            values[7], values[8], values[9], values[10], values[11], values[12]
+                            values[0], values[1], values[2], values[3], values[4], values[5],
+                            values[6], values[7], values[7], values[9], values[10], values[11]
                         )
                     )
                 else:
