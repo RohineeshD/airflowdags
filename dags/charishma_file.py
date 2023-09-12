13ecd515d4d6dfa91b64f5876e44c4a8a7b62e3f
Shyanjali4 <141653202+Shyanjali4@users.noreply.github.com>
2023-09-12 18:09:48 +0530

    Update charishma_file.py

--- dags/charishma_file.py
+++ dags/charishma_file.py
@@ -76,7 +76,10 @@ def validate_and_load_data(**kwargs):
             ssn = row[2].strip()
         
             if len(ssn) != 4:
-                print(len(ssn), type(ssn) )
+                print(len(ssn), type(ssn))
+                if len(ssn)==0:
+                    ssn ="0"
+                
                 error_msg = "Invalid SSN length; it should be 4 digits"
                 insert_error_sql = f"INSERT INTO ERROR_LOG (NAME, EMAIL, SSN, ERROR_MESSAGE) VALUES ('{row[0]}', '{row[1]}', '{ssn}', '{error_msg}')"
                 cursor.execute(insert_error_sql)
