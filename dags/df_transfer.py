27cf8d0d7eddc32319f45c0c4284407bf48ecdc6
jcharishma <141810544+jcharishma@users.noreply.github.com>
2023-09-15 12:44:13 +0530

    Update df_transfer.py

--- dags/df_transfer.py
+++ dags/df_transfer.py
@@ -3,7 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import pandas as pd
 
-def df_transfer_dag():
+def data_transfer_dag():
     # Serialize the DataFrame and store it
     df = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['Name', 'Email', 'SSN']})
     serialized_df = df.to_csv("https://github.com/jcharishma/my.repo/raw/master/sample_csv.csv")
@@ -12,10 +12,10 @@ def df_transfer_dag():
     retrieved_df = pd.read_csv("https://github.com/jcharishma/my.repo/blob/master/rep.csv")
     # Now you have the DataFrame and can use it within the same DAG
 
-dag = DAG('df_transfer_dag', start_date=datetime(2023, 1, 1))
+dag = DAG('data_transfer_dag', start_date=datetime(2023, 1, 1))
 
 transfer_task = PythonOperator(
-    task_id='df_transfer_dag',
+    task_id='data_transfer_dag',
     python_callable=transfer_df_dags,
     dag=dag,
 )
