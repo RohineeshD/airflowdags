cc65dc1048a3a58fe789906420fd1e8a8064ecd0
jcharishma <141810544+jcharishma@users.noreply.github.com>
2023-09-15 12:55:50 +0530

    Update data_transfer.py

--- dags/data_transfer.py
+++ dags/data_transfer.py
@@ -1,21 +1,21 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
-import pandas as pd
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from datetime import datetime
+# import pandas as pd
 
-def df_dag():
-    # Serialize the DataFrame and store it
-    df = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['Name', 'Email', 'SSN']})
-    serialized_df = df.to_csv("https://github.com/jcharishma/my.repo/raw/master/sample_csv.csv")
+# def df_dag():
+#     # Serialize the DataFrame and store it
+#     df = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['Name', 'Email', 'SSN']})
+#     serialized_df = df.to_csv("https://github.com/jcharishma/my.repo/raw/master/sample_csv.csv")
 
-    # Deserialize the DataFrame from the shared location
-    retrieved_df = pd.read_csv("https://github.com/jcharishma/my.repo/blob/master/rep.csv")
-    # Now you have the DataFrame and can use it within the same DAG
+#     # Deserialize the DataFrame from the shared location
+#     retrieved_df = pd.read_csv("https://github.com/jcharishma/my.repo/blob/master/rep.csv")
+#     # Now you have the DataFrame and can use it within the same DAG
 
-dag = DAG('df_dag', start_date=datetime(2023, 1, 1))
+# dag = DAG('df_dag', start_date=datetime(2023, 1, 1))
 
-transfer_task = PythonOperator(
-    task_id='df_task',
-    python_callable=df_dag,
-    dag=dag,
-)
+# transfer_task = PythonOperator(
+#     task_id='df_task',
+#     python_callable=df_dag,
+#     dag=dag,
+# )
