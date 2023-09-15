fef291be6dfc9e45127027a510b1c2d7bbbd6a66
jcharishma <141810544+jcharishma@users.noreply.github.com>
2023-09-15 18:25:08 +0530

    Update data_transfer.py

--- dags/data_transfer.py
+++ dags/data_transfer.py
@@ -1,4 +1,3 @@
-
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
@@ -44,6 +43,15 @@ def process_csv_file(**kwargs):
     if csv_link is not None:
         logging.info(f"Processing CSV file from link: {csv_link}")
         
+        # Check if the CSV link is accessible
+        import requests
+        response = requests.get(csv_link)
+        if response.status_code == 200:
+            logging.info("CSV link is accessible.")
+        else:
+            logging.error("CSV link is not accessible.")
+            return
+        
         # Use pandas to read the CSV file
         df = pd.read_csv(csv_link, encoding='utf-8')
         
@@ -52,7 +60,7 @@ def process_csv_file(**kwargs):
         
         # Log the first few rows of the filtered DataFrame
         logging.info("CSV Data:")
-        logging.info(df.to_string(index=False))  # Log the entire DataFrame
+        logging.info(df.head())  # Log the first few rows of the DataFrame
     else:
         logging.error("CSV link is None. Check if the previous task executed successfully.")
 
@@ -64,6 +72,72 @@ process_csv_file_task = PythonOperator(
     provide_context=True,
     dag=dag2,
 )
+
+# from airflow import DAG
+# from airflow.operators.python_operator import PythonOperator
+# from airflow.operators.dagrun_operator import TriggerDagRunOperator
+# from datetime import datetime
+# from airflow.utils.dates import days_ago
+# import pandas as pd
+# import logging
+
+# # Define your first DAG
+# dag1 = DAG('produce_csv_link_dag1', start_date=days_ago(1), schedule_interval=None)
+
+# # Python function to produce the CSV file link
+# def produce_csv_link():
+#     csv_link = "https://raw.githubusercontent.com/jcharishma/my.repo/master/sample_csv.csv"
+#     return csv_link
+
+# # Use PythonOperator to execute the function
+# produce_csv_link_task = PythonOperator(
+#     task_id='produce_csv_link_task1',
+#     python_callable=produce_csv_link,
+#     dag=dag1,
+# )
+
+# # Define the trigger for the second DAG
+# trigger_process_csv_file = TriggerDagRunOperator(
+#     task_id='trigger_process_csv_file_task1',
+#     trigger_dag_id="process_csv_file_dag1",  # Specify the DAG to trigger
+#     dag=dag1,
+# )
+
+# # Set up the dependency
+# produce_csv_link_task >> trigger_process_csv_file
+
+# # Define your second DAG
+# dag2 = DAG('process_csv_file_dag1', start_date=datetime(2023, 1, 1), schedule_interval=None)
+
+
+# # Python function to process the CSV file
+# def process_csv_file(**kwargs):
+#     ti = kwargs['ti']
+#     csv_link = ti.xcom_pull(task_ids='produce_csv_link_task1', key='return_value')
+    
+#     if csv_link is not None:
+#         logging.info(f"Processing CSV file from link: {csv_link}")
+        
+#         # Use pandas to read the CSV file
+#         df = pd.read_csv(csv_link, encoding='utf-8')
+        
+#         # Filter out rows with missing values in any column
+#         df = df.dropna()
+        
+#         # Log the first few rows of the filtered DataFrame
+#         logging.info("CSV Data:")
+#         logging.info(df.to_string(index=False))  # Log the entire DataFrame
+#     else:
+#         logging.error("CSV link is None. Check if the previous task executed successfully.")
+
+
+# # Use PythonOperator to execute the function
+# process_csv_file_task = PythonOperator(
+#     task_id='process_csv_file_task1',
+#     python_callable=process_csv_file,
+#     provide_context=True,
+#     dag=dag2,
+# )
 
 
 
