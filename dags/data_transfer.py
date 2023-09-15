d6de387e5740a8764c274d844ba9d29d4eba6983
jcharishma <141810544+jcharishma@users.noreply.github.com>
2023-09-15 12:51:54 +0530

    Update data_transfer.py

--- dags/data_transfer.py
+++ dags/data_transfer.py
@@ -16,6 +16,6 @@ dag = DAG('data_transfer_dag', start_date=datetime(2023, 1, 1))
 
 transfer_task = PythonOperator(
     task_id='data_transfer_task',
-    python_callable=transfer_df_dags,
+    python_callable=data_transfer_dag,
     dag=dag,
 )
