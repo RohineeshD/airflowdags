23df51357fe7a796656ab4b67be7adcb1b64a0fb
jcharishma <141810544+jcharishma@users.noreply.github.com>
2023-09-15 14:57:46 +0530

    Update data_transfer.py

--- dags/data_transfer.py
+++ dags/data_transfer.py
@@ -1,8 +1,9 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
+import pandas as pd
 
-# Define your DAG
+# Define your first DAG
 dag1 = DAG('produce_csv_link_dag', start_date=datetime(2023, 1, 1), schedule_interval=None)
 
 # Python function to produce the CSV file link
@@ -16,6 +17,31 @@ produce_csv_link_task = PythonOperator(
     python_callable=produce_csv_link,
     dag=dag1,
 )
+
+# Define your second DAG
+dag2 = DAG('process_csv_file_dag', start_date=datetime(2023, 1, 1), schedule_interval=None)
+
+# Python function to process the CSV file
+def process_csv_file(**kwargs):
+    ti = kwargs['ti']
+    csv_link = ti.xcom_pull(task_ids='produce_csv_link', key=None)
+    
+    # Use pandas to read the CSV file
+    df = pd.read_csv(csv_link, encoding='utf-8')
+
+    print(df.head())
+
+# Use PythonOperator to execute the function
+process_csv_file_task = PythonOperator(
+    task_id='process_csv_file',
+    python_callable=process_csv_file,
+    provide_context=True,
+    dag=dag2,
+)
+
+# Set up the dependency
+produce_csv_link_task >> process_csv_file_task
+
 
 
 
