d1cef801ad49142921118f026de5ba00d6ef2fe0
jcharishma <141810544+jcharishma@users.noreply.github.com>
2023-09-15 13:27:51 +0530

    Update data_transfer.py

--- dags/data_transfer.py
+++ dags/data_transfer.py
@@ -1,21 +1,67 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 import pandas as pd
 
-def df_transfer_dag():
-    # Serialize the DataFrame and store it
-    df = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['Name', 'Email', 'SSN']})
-    serialized_df = df.to_csv("https://github.com/jcharishma/my.repo/raw/master/sample_csv.csv")
+# Define your DAG
+combined_dag = DAG('combined_dag', start_date=datetime(2023, 1, 1), schedule_interval=None)
 
-    # Deserialize the DataFrame from the shared location
-    retrieved_df = pd.read_csv("https://github.com/jcharishma/my.repo/blob/master/rep.csv")
-    # Now you have the DataFrame and can use it within the same DAG
+#  produce the CSV file link in DAG1
+def produce_csv_link_dag1():
+    csv_link = "https://github.com/jcharishma/my.repo/edit/master/sample_csv.csv"
+    return csv_link
 
-dag = DAG('df_transfer_dag', start_date=datetime(2023, 1, 1), schedule_interval=None) 
+# Python function to process the CSV file  in DAG2
+def process_csv_file_dag2(**kwargs):
+    # Retrieve the CSV file link produced by DAG1
+    ti = kwargs['ti']
+    csv_link = ti.xcom_pull(task_ids='execute_produce_csv_link_dag1', key=None)
+    
+    # Use pandas to read the CSV file
+    df = pd.read_csv(csv_link)
+    
+    # Perform tasks with the dataframe
+    # For example, let's print the first few rows
+    print(df.head())
 
-transfer_task = PythonOperator(
-    task_id='df_task',
-    python_callable=df_transfer_dag,
-    dag=dag,
+# Use PythonOperator to execute the functions in DAG1 and DAG2
+execute_produce_csv_link_dag1 = PythonOperator(
+    task_id='execute_produce_csv_link_dag1',
+    python_callable=produce_csv_link_dag1,
+    dag=combined_dag,
 )
+
+execute_process_csv_file_dag2 = PythonOperator(
+    task_id='execute_process_csv_file_dag2',
+    python_callable=process_csv_file_dag2,
+    provide_context=True,
+    dag=combined_dag,
+)
+
+# Set up the dependencies
+execute_produce_csv_link_dag1 >> execute_process_csv_file_dag2
+
+
+
+
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from datetime import datetime
+# import pandas as pd
+
+# def df_transfer_dag():
+#     # Serialize the DataFrame and store it
+#     df = pd.DataFrame({'col1': [1, 2, 3], 'col2': ['Name', 'Email', 'SSN']})
+#     serialized_df = df.to_csv("https://github.com/jcharishma/my.repo/raw/master/sample_csv.csv")
+
+#     # Deserialize the DataFrame from the shared location
+#     retrieved_df = pd.read_csv("https://github.com/jcharishma/my.repo/blob/master/rep.csv")
+#     # Now you have the DataFrame and can use it within the same DAG
+
+# dag = DAG('df_transfer_dag', start_date=datetime(2023, 1, 1), schedule_interval=None) 
+
+# transfer_task = PythonOperator(
+#     task_id='df_task',
+#     python_callable=df_transfer_dag,
+#     dag=dag,
+# )
