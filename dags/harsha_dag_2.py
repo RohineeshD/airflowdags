cecdc0613185d75c7089f37a41bc2b8ca07572da
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-18 16:02:01 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.operators.python_operator import PythonOperator
+
 from airflow.utils.dates import days_ago
 import os
 
