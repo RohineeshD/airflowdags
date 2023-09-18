764462c3ff3a18834bfca9c85d2158e5f3fe551d
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-18 15:59:42 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from airflow.utils.dates import days_ago
-from airflow.hooks.snowflake_hook import SnowflakeHook
+# from airflow.hooks.snowflake_hook import SnowflakeHook
 import os
 
 # Define your DAG
