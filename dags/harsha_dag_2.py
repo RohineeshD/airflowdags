e8887750e697d3d670d7bf1ec4423fb004d4845e
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-20 20:09:36 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -1,9 +1,7 @@
 from airflow import DAG
 from airflow.sensors.filesystem import FileSensor
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
-from airflow.providers.snowflake.operators.snowflake import SnowflakeFileTransferOperator
-from airflow.providers.snowflake.operators.file_to_snowflake import SnowflakeLoadFileOperator
 from airflow.providers.snowflake.operators.snowflake import SnowflakeSQLOperator
+from airflow.providers.snowflake.transfers.local_to_snowflake import LocalFilesystemToSnowflakeOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 
@@ -37,23 +35,94 @@ file_sensor = FileSensor(
     dag=dag,
 )
 
-# Snowflake Hook for connection
-snowflake_hook = SnowflakeHook(snowflake_conn_id='air_conn')
-
-# Snowflake Operator to load the CSV file into Snowflake internal stage
-snowflake_stage_load_task = SnowflakeFileTransferOperator(
-    task_id='snowflake_stage_load_task',
+# Snowflake Operator to copy data from local directory to Snowflake stage
+copy_to_stage_task = LocalFilesystemToSnowflakeOperator(
+    task_id='copy_to_stage_task',
     schema='exusia_schema',
     table='automate_table',
-    stage='my_stage_name',  # Replace with your internal stage name
-    file_paths=[file_directory],  # List of file paths to load
+    stage='my_stage_name',  # Replace with your Snowflake stage name
+    file_path=file_directory,  # Path to the file to load
+    file_pattern='.*\.csv',  # Specify a regular expression to match CSV files
     file_format='(TYPE = "CSV" FIELD_OPTIONALLY_ENCLOSED_BY = "")',  # Specify CSV format
-    load_options='SKIP_HEADER = 1',  # Specify options like skipping header
+    column_transformation=[],  # Optional column transformations
+    snowflake_conn_id='snowflake_conn_id',
+)
+
+# Snowflake Operator to load data from Snowflake stage to Snowflake table
+snowflake_load_task = SnowflakeSQLOperator(
+    task_id='snowflake_load_task',
+    sql=f'''
+        COPY INTO automate_table
+        FROM @your_snowflake_stage
+        FILE_FORMAT = (
+            TYPE = 'CSV'
+            SKIP_HEADER = 1
+            FIELD_OPTIONALLY_ENCLOSED_BY = ''
+        );
+    ''',
     snowflake_conn_id='snowflake_conn_id',
 )
 
 # Set task dependencies
-file_sensor >> snowflake_stage_load_task
+file_sensor >> copy_to_stage_task >> snowflake_load_task
+
+
+# from airflow import DAG
+# from airflow.sensors.filesystem import FileSensor
+# from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+# from airflow.providers.snowflake.operators.snowflake import SnowflakeFileTransferOperator
+# from airflow.providers.snowflake.operators.file_to_snowflake import SnowflakeLoadFileOperator
+# from airflow.providers.snowflake.operators.snowflake import SnowflakeSQLOperator
+# from airflow.utils.dates import days_ago
+# from datetime import datetime, timedelta
+
+# start_date = days_ago(1)
+
+# default_args = {
+#     'owner': 'airflow',
+#     'depends_on_past': False,
+#     'retries': 1,
+#     'retry_delay': timedelta(minutes=5),
+# }
+
+# dag = DAG(
+#     'snowflake_file_load_dag',
+#     default_args=default_args,
+#     start_date=start_date,
+#     description='DAG to load CSV files into Snowflake',
+#     schedule_interval=None,  # Set the schedule interval according to your requirements
+# )
+
+# # Define the directory where CSV files will arrive
+# file_directory = 'C:/Users/User/Desktop/load'
+
+# # Create a FileSensor to detect the presence of new CSV files
+# file_sensor = FileSensor(
+#     task_id='file_sensor_task',
+#     filepath=file_directory,
+#     poke_interval=10,  # Check every 10 seconds if a new file has arrived
+#     timeout=3600,  # Stop checking after 1 hour
+#     mode='poke',
+#     dag=dag,
+# )
+
+# # Snowflake Hook for connection
+# snowflake_hook = SnowflakeHook(snowflake_conn_id='air_conn')
+
+# # Snowflake Operator to load the CSV file into Snowflake internal stage
+# snowflake_stage_load_task = SnowflakeFileTransferOperator(
+#     task_id='snowflake_stage_load_task',
+#     schema='exusia_schema',
+#     table='automate_table',
+#     stage='my_stage_name',  # Replace with your internal stage name
+#     file_paths=[file_directory],  # List of file paths to load
+#     file_format='(TYPE = "CSV" FIELD_OPTIONALLY_ENCLOSED_BY = "")',  # Specify CSV format
+#     load_options='SKIP_HEADER = 1',  # Specify options like skipping header
+#     snowflake_conn_id='snowflake_conn_id',
+# )
+
+# # Set task dependencies
+# file_sensor >> snowflake_stage_load_task
 
 
 
