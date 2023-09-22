517605395cddaa078b0233b4b57ac127d64b3282
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-22 12:13:54 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -29,7 +29,7 @@ dag = DAG(
 # # Define the directory where CSV files will arrive
 # file_directory = '"C:/Users/User/Desktop/load"'
 
-file_directory  = 'Downloaded_CSV_TABLE.csv'
+file_directory  = 'downloade_csv.csv'
 def list_files():
     try:
         with open(file_directory, 'r') as file:
