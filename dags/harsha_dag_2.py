a97b1c57ffed6a2567b948c8d7759518894f9f9f
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-25 13:33:25 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -50,7 +50,7 @@ with DAG('load_data_to_snowflake',
 
         try:
             logging.info(f"Uploading CSV file: {file_path} to Snowflake stage: {snowflake_stage}")
-            snowflake_hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+            snowflake_hook = SnowflakeHook(snowflake_conn_id='new_conn')
 
             # Define the Snowflake SQL statement to load data (replace with your SQL)
             sql = f"""
