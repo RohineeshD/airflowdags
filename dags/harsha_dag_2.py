2701c63feb2e7bbf0e37446a9d46a4d7b9874e39
mukkellaharsha <141811342+mukkellaharsha@users.noreply.github.com>
2023-09-25 13:36:40 +0530

    Update harsha_dag_2.py

--- dags/harsha_dag_2.py
+++ dags/harsha_dag_2.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.sensors.http_sensor import HttpSensor
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
-from airflow.sensors.http_sensor import GitHubFileSensor
+from airflow.sensors import GitHubFileSensor
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 
 # Import statements for other dependencies (if any)
